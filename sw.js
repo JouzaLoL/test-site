7a87d1becb1a4083696f83ac1df830a5e2f6fbcf
Josef Vacek <vacekj@outlook.com>
2018-03-30 11:44:28 +0200

    Updates

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ var __wpo = {
       "./robots.txt",
       "./img/favicon.png",
       "./tv.html",
-      "./manifest.txt",
+      "./manifest.json",
       "./",
       "./tv.css",
       "./tv.js",
@@ -23,16 +23,16 @@ var __wpo = {
     "32ecc19b8c505c3149ad535431b75c3d1c061327": "./robots.txt",
     "cbaa6bd8048a9e37bf5eb9fb5abff2f1ee43085b": "./img/favicon.png",
     "b6b0abf0dc4fa11eef693bf3ffc8441a699bb717": "./tv.html",
-    "6456cce6abf61f818747f39cb5000c9e66e0fe6c": "./manifest.txt",
+    "6456cce6abf61f818747f39cb5000c9e66e0fe6c": "./manifest.json",
     "8f0a1a73a2ace8bff70016037b9bb9aa62889ba2": "./",
     "0f7dd98965509924b58676c5610768a9335eecd6": "./tv.css",
     "021d5a3e9b35be7e6cf2dfa63b744e025c00710c": "./tv.js",
     "3bb3af825af3941f80539f9a7388a21334f9e317": "./index.css",
-    "2731e9e40047491b982b24257f12adcb3dad20ee": "./index.js"
+    "5b63ca5bade360043b4295c2d9d67ca84302c37a": "./index.js"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2018-3-30 11:33:26",
+  "version": "2018-3-30 11:44:05",
   "name": "webpack-offline",
   "pluginVersion": "4.9.0",
   "relativePaths": true
