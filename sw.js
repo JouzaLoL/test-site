1fb9807b73229f65740e04f6bef8461e8d60d5ea
Josef Vacek <vacekj@outlook.com>
2018-03-30 16:00:13 +0200

    Updates

--- sw.js
+++ sw.js
@@ -44,11 +44,11 @@ var __wpo = {
     "0f7dd98965509924b58676c5610768a9335eecd6": "./tv.css",
     "7cd13e7e5e8666e4d42fcaee97aa8a6ee95a0462": "./tv.js",
     "3bb3af825af3941f80539f9a7388a21334f9e317": "./index.css",
-    "5daaccac8b814d138cd250111ca182bb307ac99e": "./index.js"
+    "50b36915eda4ce8c309e7b8320c5b59199ba9380": "./index.js"
   },
   "strategy": "changed",
   "responseStrategy": "cache-first",
-  "version": "2018-3-30 15:58:02",
+  "version": "2018-3-30 16:00:05",
   "name": "webpack-offline",
   "pluginVersion": "4.9.0",
   "relativePaths": true
